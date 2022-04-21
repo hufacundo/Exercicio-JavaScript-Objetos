1bcb195dbd53c572a8a0f4322d0d76f74b091cf5
Hugo Facundo <89493574+hufacundo@users.noreply.github.com>
2022-04-21 00:28:31 -0300

    Add files via upload

---
+++ 03Objetos.js
@@ -0,0 +1,177 @@
+var pessoa01 = new Object();
+pessoa01.nome = pessoa01.nome;
+parseInt(pessoa01.idade);
+pessoa01.filiacao = Array(2).fill(0);
+
+var endereco01 = new Object();
+endereco01.rua = endereco01.rua;
+endereco01.cidade = endereco01.cidade;
+endereco01.estado = endereco01.estado;
+
+function dadosCompletos01() {
+    if (pessoa01.nome == "" || pessoa01.nome == null) {
+        console.log("Não possui dados!\n");
+    }
+    if (pessoa01.idade == "" || pessoa01.idade == null) {
+        console.log("Não possui dados!\n");
+    }
+    if (pessoa01.filiacao[0] == "" || pessoa01.filiacao[0] == null) {
+        console.log("Não possui dados!\n");
+    }
+    if (pessoa01.filiacao[1] == "" || pessoa01.filiacao[1] == null) {
+        console.log("Não possui dados!\n");
+    }
+    if (endereco01.rua == "" || endereco01.rua == null) {
+        console.log("Não possui dados!\n");
+    }       
+    if (endereco01.cidade == "" || endereco01.cidade == null) {
+        console.log("Não possui dados!\n");
+    }
+    if (endereco01.estado == "" || endereco01.estado == null) {
+        console.log("Não possui dados!\n");
+    }  
+    else {
+      console.log("Os dados foram conferidos e estão todos corretos!!!\n");
+    }
+} 
+function total01() {
+    pessoa01.nome = "Coriolano Filho"
+    pessoa01.idade = "25"
+    pessoa01.filiacao[0] = "Coriolando";
+    pessoa01.filiacao[1] = "Maria Coriolanda";
+    endereco01.rua = "Rua da Areia";
+    endereco01.cidade = "João Pessoa";
+    endereco01.estado = "Paraíba";
+
+    console.log("Nome: " + pessoa01.nome + "\nTipo: " + typeof(pessoa01.nome));
+    console.log("Idade: " + pessoa01.idade + "\nTipo: " + typeof(pessoa01.idade));
+    console.log("Nome do pai: " + pessoa01.filiacao[0] + "\nTipo: " + typeof(pessoa01.filiacao[0]));
+    console.log("Nome da mãe: " + pessoa01.filiacao[1] + "\nTipo: " + typeof(pessoa01.filiacao[1]));
+    console.log("Nome da rua: " + endereco01.rua + "\nTipo: " + typeof(endereco01.rua));
+    console.log("Nome da cidade: " + endereco01.cidade + "\nTipo: " + typeof(endereco01.cidade));
+    console.log("Nome do estado: " + endereco01.estado + "\nTipo: " + typeof(endereco01.estado));
+
+    dadosCompletos01();
+}
+
+total01();
+
+
+var pessoa02 = new Object();
+pessoa02.nome = pessoa02.nome;
+parseInt(pessoa02.idade);
+pessoa02.filiacao = Array(2).fill(0);
+
+var endereco02 = new Object();
+endereco02.rua = endereco02.rua;
+endereco02.cidade = endereco02.cidade;
+endereco02.estado = endereco02.estado;
+
+function dadosCompletos02() {
+    if (pessoa02.nome == "" || pessoa02.nome == null) {
+        console.log("Não possui dados!\n");
+    }
+    if (pessoa02.idade == "" || pessoa02.idade == null) {
+        console.log("Não possui dados!\n");
+    }
+    if (pessoa02.filiacao[0] == "" || pessoa02.filiacao[0] == null) {
+        console.log("Não possui dados!\n");
+    }
+    if (pessoa02.filiacao[1] == "" || pessoa02.filiacao[1] == null) {
+        console.log("Não possui dados!\n");
+    }
+    if (endereco02.rua == "" || endereco02.rua == null) {
+        console.log("Não possui dados!\n");
+    }
+    if (endereco02.cidade == "" || endereco02.cidade == null) {
+        console.log("Não possui dados!\n");
+    }
+    if (endereco02.estado == "" || endereco02.estado == null) {
+        console.log("Não possui dados!\n");
+    }  
+    else {
+        console.log("Os dados foram conferidos e estão todos corretos!!!\n");     
+    }
+} 
+
+function total02() {
+    pessoa02.nome = "Guilherme Facundo";
+    pessoa02.idade = "2";
+    pessoa02.filiacao[0] = "Hugo Facundo";
+    pessoa02.filiacao[1] = "Aline Facundo";
+    endereco02.rua = "Rua da Aurora";
+    endereco02.cidade = "João Pessoa";
+    endereco02.estado = "Paraíba";
+
+    console.log("Nome: " + pessoa02.nome + "\nTipo: " + typeof(pessoa02.nome));
+    console.log("Idade: " + pessoa02.idade + "\nTipo: " + typeof(pessoa02.idade));
+    console.log("Nome do pai: " + pessoa02.filiacao[0] + "\nTipo: " + typeof(pessoa02.filiacao[0]));
+    console.log("Nome da mãe: " + pessoa02.filiacao[1] + "\nTipo: " + typeof(pessoa02.filiacao[1]));
+    console.log("Nome da rua: " + endereco02.rua + "\nTipo: " + typeof(endereco02.rua));
+    console.log("Nome da cidade: " + endereco02.cidade + "\nTipo: " + typeof(endereco02.cidade));
+    console.log("Nome do estado: " + endereco02.estado + "\nTipo: " + typeof(endereco02.estado));
+
+    dadosCompletos02();
+}
+
+total02();
+
+
+var pessoa03 = new Object();
+pessoa03.nome = pessoa03.nome;
+parseInt(pessoa03.idade);
+pessoa03.filiacao = Array(2).fill(0);
+
+var endereco03 = new Object();
+endereco03.rua = endereco03.rua;
+endereco03.cidade = endereco03.cidade;
+endereco03.estado = endereco03.estado;
+
+function dadosCompletos03() {
+    if (pessoa03.nome == "" || pessoa03.nome == null) {
+        console.log("Não possui dados!\n");
+    }
+    if (pessoa03.idade == "" || pessoa03.idade == null) {
+        console.log("Não possui dados!\n");
+    }
+    if (pessoa03.filiacao[0] == "" || pessoa03.filiacao[0] == null) {
+        console.log("Não possui dados!\n");
+    }
+    if (pessoa03.filiacao[1] == "" || pessoa03.filiacao[1] == null) {
+        console.log("Não possui dados!\n");
+    }
+    if (endereco03.rua == "" || endereco03.rua == null) {
+        console.log("Não possui dados!\n");
+    }
+    if (endereco03.cidade == "" || endereco03.cidade == null) {
+        console.log("Não possui dados!\n");
+    }
+    if (endereco03.estado == "" || endereco03.estado == null) {
+        console.log("Não possui dados!\n");
+    }  
+    else {
+        console.log("Os dados foram conferidos e estão todos corretos!!!\n");     
+    }
+} 
+
+function total03() {
+    pessoa03.nome = "Alice Oliveira";
+    pessoa03.idade = "10";
+    pessoa03.filiacao[0] = "Dimas Patriota";
+    pessoa03.filiacao[1] = "Kiara Fernandes";
+    endereco03.rua = "Rua da Alvorada";
+    endereco03.cidade = "Fortaleza";
+    endereco03.estado = "Ceará";
+
+    console.log("Nome: " + pessoa03.nome + "\nTipo: " + typeof(pessoa03.nome));
+    console.log("Idade: " + pessoa03.idade + "\nTipo: " + typeof(pessoa03.idade));
+    console.log("Nome do pai: " + pessoa03.filiacao[0] + "\nTipo: " + typeof(pessoa03.filiacao[0]));
+    console.log("Nome da mãe: " + pessoa03.filiacao[1] + "\nTipo: " + typeof(pessoa03.filiacao[1]));
+    console.log("Nome da rua: " + endereco03.rua + "\nTipo: " + typeof(endereco03.rua));
+    console.log("Nome da cidade: " + endereco03.cidade + "\nTipo: " + typeof(endereco03.cidade));
+    console.log("Nome do estado: " + endereco03.estado + "\nTipo: " + typeof(endereco03.estado));
+
+    dadosCompletos03();
+}
+
+total03();
